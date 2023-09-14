266a66c7b5a1a100c4b7e85795a42f4e88acc816
nichardo <137659721+nichardo@users.noreply.github.com>
2023-09-14 14:46:26 -0300

    Update about.jsx

--- src/assets/about.jsx
+++ src/assets/about.jsx
@@ -10,15 +10,15 @@ function about() {
             <h1>El Menticiero</h1>
 
             <p align="justify">
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit ullam sunt eveniet enim libero quas sequi expedita est beatae voluptatem quidem, ipsa quam repellendus odio voluptates eius assumenda! Vel, voluptatibus?
+                El Menticiero es un sitio de noticias que ofrece información veraz, actualizada y entretenida sobre Cuba y El Mundo.
             </p>
 
             <p align="justify">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Id ullam harum pariatur at ex et rem molestiae rerum, porro labore iusto deleniti consectetur? Animi, voluptate nemo perspiciatis quisquam magni modi.
+                Aquí encontrarás reportajes, entrevistas, análisis y opinión sobre los temas más relevantes de la política, la economía, la cultura, la ciencia y el deporte.
             </p>
 
             <p align="justify">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia excepturi consectetur aspernatur sequi at saepe sapiente ducimus, dolore recusandae deleniti esse facilis facere cumque error aut quia eaque. Fugiat, quia!
+                El Menticiero te invita a pensar, a cuestionar y a participar en el debate público con una mirada crítica y constructiva. El Menticiero es el sitio de noticias que no te miente.
             </p>
             
         </div>
