576440b404f263840a24b922d2de0fd226f533a6
nichardo <137659721+nichardo@users.noreply.github.com>
2023-09-16 16:26:36 -0300

    Update header.js

--- src/assets/header.js
+++ src/assets/header.js
@@ -4,11 +4,11 @@ const header = () => {
   return (
     <div className="nav-container">
       <div>
-          <a className="cta-btn" to href="/">
+          <a to href="/">
               Home
           </a>
       </div>
-      <a href="https://drive.google.com/file/d/1yvp9WXV8eGmJf--r_87088j-Twso4Xk-/view?usp=sharing" target="_blank" className="cta-btn">Resumen</a>
+      <a href="/" target="_blank" className="cta-btn">Resumen</a>
     </div>
   )
 }
